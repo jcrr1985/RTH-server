229c1219b882a91be0fd113910479daf1991114f
julio rodriguez <julio.rodriguez-ext@talan.com>
2024-01-09 18:59:02 +0500

    enhancing cors

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ const axios = require("axios");
 const app = express();
 const port = 5000;
 
-app.use(cors());
+app.use(express.json({ limit: "15mb" })); // Permite que el servidor recibe request de tipo Json.
+app.use(express.urlencoded({ extended: true, limit: "15mb" })); // Permite recibir request de tipo "multimedia"
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      callback(null, true);
+    },
+
+    credentials: true,
+
+    methods: "POST,GET,PUT,OPTIONS,DELETE",
+  })
+);
 
 app.get("/places", async (req, res) => {
   // const apiUrl = req.query.apiUrl;
