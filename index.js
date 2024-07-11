9ec01b5f95484ebbc47c8995b5be1cb70e50d2da
julio rodriguez <julio.rodriguezr1@gmail.com>
2024-07-12 04:26:23 +0500

    adding cors manually2

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ app.use(function (req, res, next) {
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true, limit: "15mb" }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
