759457e2707e765355bdb8d4f945215052b24dcd
julio rodriguez <julio.rodriguezr1@gmail.com>
2024-07-11 23:56:37 +0500

    adding all methods for cars

--- index.js
+++ index.js
@@ -97,6 +97,85 @@ app.post("/upload", async (req, res) => {
   }
 });
 
+// Obtener todos los coches
+app.get("/cars", async (req, res) => {
+  try {
+    const cars = await Car.find();
+    res.json(cars);
+  } catch (err) {
+    console.log("err::", err);
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Obtener un coche por ID
+app.get("/cars/:id", async (req, res) => {
+  try {
+    const car = await Car.findById(req.params.id);
+    if (!car) {
+      res.status(404).json({ message: "car not found" });
+    }
+    res.json(car);
+  } catch (error) {
+    console.log("error", error);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Actualizar un coche por ID
+app.put("/cars/:id", async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      hireDate,
+      department,
+      phone,
+      address,
+      isActive,
+      forSell,
+    } = req.body;
+    const updatedCar = await Car.findByIdAndUpdate(
+      req.params.id,
+      {
+        firstName,
+        lastName,
+        hireDate,
+        department,
+        phone,
+        address,
+        isActive,
+        forSell,
+      },
+      { new: true }
+    );
+
+    console.log("updatedCar en server", updatedCar);
+
+    if (!updatedCar) {
+      res.status(404).json({ message: "car not found" });
+    }
+    res.json(updatedCar);
+  } catch (error) {
+    console.log("error.message", error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Eliminar un coche por ID
+app.delete("/cars/:id", async (req, res) => {
+  try {
+    const deletedCar = await Car.findByIdAndDelete(req.params.id);
+    if (!deletedCar) {
+      return res.status(404).json({ message: "Car not found" });
+    }
+    res.json(deletedCar);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Error deleting car" });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
