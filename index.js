f9c0791d9b7ccc6b8f0358fc72055acec71d5a29
julio rodriguez <julio.rodriguezr1@gmail.com>
2024-07-17 00:31:43 +0500

    deleting mongo connection

--- index.js
+++ index.js
@@ -23,24 +23,6 @@ app.use(function (req, res, next) {
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true, limit: "15mb" }));
 
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on("error", (error) => console.error("MongoDB connection error:", error));
-db.once("open", () => console.log("Connected to MongoDB"));
-
-const feedbackSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  country: { type: String, required: true },
-  comment: { type: String, required: true },
-});
-
-const Feedback = mongoose.model("feedback", feedbackSchema);
-
 app.get("/places", async (req, res) => {
   const apiKey = "AIzaSyDlqhte9y0XRMqlkwF_YJ6Ynx8HQrNyF3k";
   const apiUrl = `${req.query.apiUrl}&key=${apiKey}`;
