0a739e4fc74aa2ab96ebd0497ae7f0fd6398e4ad
julio rodriguez <julio.rodriguezr1@gmail.com>
2024-08-05 21:06:30 +0500

    fixing cors

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-const url =
-  "mongodb+srv://jcrr1985:Tumama4$@cluster0.zi7qsgn.mongodb.net/fullapp";
 app.use(express.json());
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  next();
-});
-
+app.use(cors());
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true, limit: "15mb" }));
 
@@ -37,7 +25,7 @@ app.get("/places", async (req, res) => {
 });
 
 app.get("/clinics", async (req, res) => {
-  const nameOfClinicValue = req.query.nameOfClinicValue;
+  const nameOfClinicValue = req.query.name;
   const apiKey = "AIzaSyDlqhte9y0XRMqlkwF_YJ6Ynx8HQrNyF3k";
   const apiUrl = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${nameOfClinicValue}&inputtype=textquery&fields=name,formatted_address,rating,opening_hours,geometry,place_id&key=${apiKey}`;
 
@@ -50,6 +38,44 @@ app.get("/clinics", async (req, res) => {
     res.status(500).json({ error: "Error interno del servidor" });
   }
 });
+const { OpenAI } = require("openai");
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  baseURL: "https://api.aimlapi.com",
+});
+
+app.get("/chat", async (req, res) => {
+  try {
+    console.log("chat");
+    let response = await main();
+    res.json({ message: response });
+  } catch (error) {
+    console.error("Error during /chat request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    model: "mistralai/Mistral-7B-Instruct-v0.2",
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant.",
+      },
+      {
+        role: "user",
+        content: "Hola, ¿cómo estás?",
+      },
+    ],
+    temperature: 0.7,
+    max_tokens: 128,
+  });
+  console.log("AI/ML API:\n", completion.choices[0].message.content);
+
+  return completion.choices[0].message.content;
+}
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
